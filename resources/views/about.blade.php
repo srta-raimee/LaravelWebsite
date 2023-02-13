6c985adaf4f9710f1d005e363a5b26d992ddb5b2
Larissa Raimee Gomes <srta.raimee@gmail.com>
2023-02-13 15:55:46 -0300

    Beggining

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+@section('title', 'About Mee')
+@section('content')
+
+I'm only a normal girl with a normal life
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
+    <title>@yield('title')</title>
+</head>
+<body>
+    @yield('content')
+    <footer></br><strong>Raimee Studios &copy; 2023</strong></footer>
+</body>
+</html>
